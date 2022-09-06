86c5f16c647fb9bfa0a10f6dc31b99af5e25ca86
HyeJun <solar3070@naver.com>
2022-09-06 14:04:01 +0900

    PG/Lv1/완주하지 못한 선수 - 3

--- PG/Lv1/완주하지 못한 선수.js
+++ PG/Lv1/완주하지 못한 선수.js
@@ -1,14 +1,14 @@
 function solution(participant, completion) {
   let answer = "";
-  for (let i = 0; i < participant.length; i++) {
-    const idx = completion.indexOf(participant[i]);
-    if (idx !== -1) {
-      completion.splice(idx, 1);
-    } else {
+
+  participant.sort();
+  completion.sort();
+
+  for (let i = 0; i < participant.length; i++)
+    if (participant[i] !== completion[i]) {
       answer = participant[i];
       break;
     }
-  }
 
   return answer;
 }
