4e32988645e0da3984e5caa6aeb40d12c71c105f
HyeJun <solar3070@naver.com>
2022-09-06 13:09:56 +0900

    PG/Lv1/체육복 - 2

--- PG/Lv1/체육복.js
+++ PG/Lv1/체육복.js
@@ -1,27 +1,20 @@
 function solution(n, lost, reserve) {
-  let answer = n - lost.length;
-  reserve.sort((a, b) => a - b);
-  lost.sort((a, b) => a - b);
-
-  const tmp = [...reserve]
-  for (let i = 0; i < tmp.length; i++) {
-      const idx = lost.indexOf(tmp[i]);
-      if (idx !== -1) {
-          lost.splice(idx, 1);
-          reserve.splice(reserve.indexOf(tmp[i]), 1);
-          answer += 1;
-      }
-  }
+  const newLost = lost.filter(l => !reserve.includes(l));
+  const newReserve = reserve.filter(r => !lost.includes(r));
+  let answer = n - newLost.length;
   
-  for (let i = 0; i < reserve.length; i++) {
+  newLost.sort((a, b) => a - b);
+  newReserve.sort((a, b) => a - b);
+  
+  for (let r of newReserve) {
       let idx = -1;
-      for (let j = 0; j < lost.length; j++)
-          if (Math.abs(reserve[i] - lost[j]) <= 1) {
-              idx = j
+      for (let i = 0; i < newLost.length; i++)
+          if (Math.abs(r - newLost[i]) <= 1) {
+              idx = i
               break;
           }
       if (idx !== -1) {
-          lost.splice(idx, 1);
+          newLost.splice(idx, 1);
           answer += 1;
       }
   }
