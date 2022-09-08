822ca64c171c9488aced890b12bf645a1463756e
HyeJun <solar3070@naver.com>
2022-09-08 16:41:09 +0900

    PG/Lv1/두 정수 사이의 합 - 2

--- PG/Lv1/두 정수 사이의 합.js
+++ PG/Lv1/두 정수 사이의 합.js
@@ -1,8 +1,3 @@
 function solution(a, b) {
-  let answer = 0;
-  
-  for (let n = Math.min(a, b); n <= Math.max(a, b); n++)
-      answer += n
-
-  return answer;
+  return (a + b) * (Math.abs(a - b) + 1) / 2;
 }
