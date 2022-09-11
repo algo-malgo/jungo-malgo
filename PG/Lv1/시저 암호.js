acff402a391a1b8a378a5b03c97386d011ede557
HyeJun <solar3070@naver.com>
2022-09-11 16:27:18 +0900

    PG/Lv1/시저 암호 - 2

--- PG/Lv1/시저 암호.js
+++ PG/Lv1/시저 암호.js
@@ -2,10 +2,14 @@ function solution(s, n) {
 
   const arr = Array.from(s).map(ch => {
       if (ch === ' ') return ch;
-      const check = ch === ch.toUpperCase() ? 90 : 122;
-      const tmpCh = ch.charCodeAt(0) + n;
-      const newCh = tmpCh > check ? tmpCh - 26 : tmpCh;
-      return String.fromCharCode(newCh);
+      const charCode = ch.charCodeAt(0);
+      if (ch === ch.toUpperCase()) {
+          const ACode = 'A'.charCodeAt(0);
+          return String.fromCharCode(newAscii = ACode + (charCode - ACode + n) % 26);
+      } else {
+          const aCode = 'a'.charCodeAt(0);
+          return String.fromCharCode(newAscii = aCode + (charCode - aCode + n) % 26);
+      }
   })
 
   return arr.join('');
