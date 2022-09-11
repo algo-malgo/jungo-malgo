8952f8993d566a84df09555944eca8e3f827f209
HyeJun <solar3070@naver.com>
2022-09-11 18:58:02 +0900

    PG/Lv1/제일 작은 수 제거하기 - 2

--- PG/Lv1/제일 작은 수 제거하기.js
+++ PG/Lv1/제일 작은 수 제거하기.js
@@ -1,5 +1,4 @@
 function solution(arr) {
-  const min = [...arr].sort((a, b) => b - a)[arr.length - 1];
-  arr.splice(arr.indexOf(min), 1);
+  arr.splice(arr.indexOf(Math.min(...arr)), 1);
   return !arr.length ? [-1] : arr;
 }
