acb4bea08b493415224b1ff4b7ad2454f72e570a
HyeJun <solar3070@naver.com>
2022-09-12 21:48:32 +0900

    PG/Lv1/핸드폰 번호 가리기 - 2

--- PG/Lv1/핸드폰 번호 가리기.js
+++ PG/Lv1/핸드폰 번호 가리기.js
@@ -1,4 +1,3 @@
 function solution(phone_number) {
-  const len = phone_number.length;
-  return '*'.repeat(len - 4) + phone_number.slice(len - 4, len);;
+  return '*'.repeat(phone_number.length - 4) + phone_number.slice(-4);;
 }
