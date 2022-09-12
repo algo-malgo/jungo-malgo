acd827310ad9f6d3332cb76fbe7b524e6a899dae
HyeJun <solar3070@naver.com>
2022-09-12 23:29:51 +0900

    PG/Lv1/행렬의 덧셈 - 2

--- PG/Lv1/행렬의 덧셈.js
+++ PG/Lv1/행렬의 덧셈.js
@@ -2,10 +2,9 @@ function solution(arr1, arr2) {
   let answer = [];
   
   for (let i = 0; i < arr1.length; i++) {
-      let tmp = [];
+      answer[i] = [];
       for (let j = 0; j < arr1[0].length; j++) 
-          tmp.push(arr1[i][j] + arr2[i][j]);
-      answer.push(tmp);
+          answer[i].push(arr1[i][j] + arr2[i][j]);
   }
   
   return answer;
