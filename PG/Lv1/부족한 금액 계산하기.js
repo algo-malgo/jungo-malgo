48d57b2f217c8233d89107be590301d281cb2751
HyeJun <solar3070@naver.com>
2022-09-13 22:03:34 +0900

    PG/Lv1/부족한 금액 계산하기 - 2

--- PG/Lv1/부족한 금액 계산하기.js
+++ PG/Lv1/부족한 금액 계산하기.js
@@ -1,6 +1,4 @@
 function solution(price, money, count) {
-  let answer = 0;
-  for (let i = 1; i <= count; i++)
-      answer += price * i;
+  let answer = (price + price * count) * count / 2;
   return answer > money ? answer - money : 0;
 }
