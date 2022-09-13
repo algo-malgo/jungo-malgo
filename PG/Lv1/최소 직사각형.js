47dfb938d7e32502e81b8d5a05007c8de22e35bd
HyeJun <solar3070@naver.com>
2022-09-13 21:40:37 +0900

    PG/Lv1/최소 직사각형 - 2

--- PG/Lv1/최소 직사각형.js
+++ PG/Lv1/최소 직사각형.js
@@ -1,9 +1,9 @@
 function solution(sizes) {
-  const tmp = sizes.map(size => size.sort((a, b) => a - b));
-  let max1 = 0, max2 = 0;
-  for (let i = 0; i < tmp.length; i++) {
-      if (tmp[i][0] > max1) max1 = tmp[i][0];
-      if (tmp[i][1] > max2) max2 = tmp[i][1];
-  }    
-  return max1 * max2;
+  let m1 = 0, m2 = 0;
+  sizes.forEach(size => {
+      const [a, b] = size.sort((a, b) => a - b);
+      if (a > m1) m1 = a;
+      if (b > m2) m2 = b;
+  })  
+  return m1 * m2;
 }
