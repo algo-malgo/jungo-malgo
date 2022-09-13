9acbc6f57e33e673578d9f3a96d4dac178a32c6c
HyeJun <solar3070@naver.com>
2022-09-13 23:36:01 +0900

    PG/Lv1/두 개 뽑아서 더하기

---
+++ PG/Lv1/두 개 뽑아서 더하기.js
@@ -0,0 +1,7 @@
+function solution(numbers) {
+  let answer = [];
+  for (let i = 0; i < numbers.length; i++) 
+      for (let j = i + 1; j < numbers.length; j++) 
+          answer.push(numbers[i] + numbers[j]);
+  return [...new Set(answer)].sort((a, b) => a - b);
+}
