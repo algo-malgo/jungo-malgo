49b7558a46a1e04600d530ee9de2dd7a19b42f6e
HyeJun <solar3070@naver.com>
2022-09-15 23:28:43 +0900

    PG/Lv1/성격 유형 검사하기 - 2

--- PG/Lv1/성격 유형 검사하기.js
+++ PG/Lv1/성격 유형 검사하기.js
@@ -2,15 +2,12 @@ function solution(survey, choices) {
   let answer = '';
   let score = {'R': 0, 'T': 0, 'C': 0, 'F': 0, 'J': 0, 'M': 0, 'A': 0, 'N': 0};
   for (let i = 0; i < survey.length; i++) {
-      const [t1, t2] = survey[i].split('');
-      if (choices[i] > 4) 
-          score[t2] += choices[i] % 4;
-      else 
-          score[t1] += 4 - choices[i];
-  }
+    const [t1, t2] = survey[i];
+    score[choices[i] > 4 ? t2 : t1] += Math.abs(4 - choices[i]);
+}
 
-  const val = Object.keys(score);
-  for (let i = 0; i < 8; i += 2)
-      answer += score[val[i]] >= score[val[i + 1]] ? val[i] : val[i + 1]
+  const t = Object.keys(score);
+  for (let i = 0; i < Object.keys(score).length; i += 2)
+      answer += score[t[i]] >= score[t[i + 1]] ? t[i] : t[i + 1]
   return answer;
 }
