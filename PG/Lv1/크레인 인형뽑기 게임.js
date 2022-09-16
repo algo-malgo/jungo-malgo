4f8e25bd9275f909b84cfc77ff81bde988ea0f5c
HyeJun <solar3070@naver.com>
2022-09-16 18:08:56 +0900

    PG/Lv1/크레인 인형뽑기 게임 - 2

--- PG/Lv1/크레인 인형뽑기 게임.js
+++ PG/Lv1/크레인 인형뽑기 게임.js
@@ -1,16 +1,20 @@
 function solution(board, moves) {
   let answer = 0;
   let bucket = [];
-  for (let i = 0; i < moves.length; i++) 
-      for (let j = 0; j < board.length; j++) 
-          if (board[j][moves[i] - 1] !== 0) {
-              bucket.push(board[j][moves[i] - 1]);
-              board[j][moves[i] - 1] = 0;
-              if (bucket.length >= 2 && bucket.slice(-1)[0] === bucket.slice(-2, -1)[0]) {
-                  bucket = bucket.slice(0, -2);
+  for (let i = 0; i < moves.length; i++) {
+      for (let j = 0; j < board.length; j++) {
+          const item = board[j][moves[i] - 1];
+          if (item) {
+              if (item === bucket.slice(-1)[0]) {
+                  bucket.pop();
                   answer += 2;
+              } else {
+                  bucket.push(item);  
               }
+              board[j][moves[i] - 1] = 0;
               break;
           }
+      }
+  }
   return answer;
 }
