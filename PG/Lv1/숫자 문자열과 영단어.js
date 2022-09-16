93be1e87cf94ee832471c7c34e7202addccfec19
HyeJun <solar3070@naver.com>
2022-09-16 13:11:10 +0900

    PG/Lv1/숫자 문자열과 영단어 - 2

--- PG/Lv1/숫자 문자열과 영단어.js
+++ PG/Lv1/숫자 문자열과 영단어.js
@@ -1,8 +1,7 @@
 function solution(s) {
-  const num = {'zero': 0, 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9};
-  const word = Object.keys(num);
-  for (let i = 0; i < word.length && Number.isNaN(+s); i++) 
-      while (s.indexOf(word[i]) !== -1) 
-          s = s.replace(word[i], num[word[i]]);
+  const num = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  for (let i = 0; i < num.length && Number.isNaN(+s); i++) 
+      while (s.indexOf(num[i]) !== -1) 
+          s = s.replace(num[i], i);
   return +s;
 }
