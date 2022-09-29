237b026771c41513b9c8eb5902d682a3dbc51e96
HyeJun <solar3070@naver.com>
2022-09-30 00:44:31 +0900

    PG/Lv2/피보나치 수 - 2

--- PG/Lv2/피보나치 수.js
+++ PG/Lv2/피보나치 수.js
@@ -1,15 +1,9 @@
-function fibo(n, memo) {
-  if (n === 0 || n === 1) return n;
-
-  if (memo[n - 1] === -1) memo[n - 1] = fibo(n - 1, memo);
-  if (memo[n - 2] === -1) memo[n - 2] = fibo(n - 2, memo);
-  
-  return (memo[n - 1] % 1234567 + memo[n - 2] % 1234567) % 1234567;
-}
-
 function solution(n) {
-  let memo = Array.from({length: n}, () => -1);
-  memo[0] = 0;
-  memo[1] = 1;
-  return fibo(n, memo);
+  let v1 = 0, v2 = 1;
+  for (let i = 2; i <= n; i++) {
+      let sum = (v1 % 1234567 + v2 % 1234567) % 1234567;
+      v1 = v2 % 1234567;
+      v2 = sum;
+  }
+  return v2;
 }
