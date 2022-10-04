f0b0e3413b6e1173f21713d34ad389034c6e6795
HyeJun <solar3070@naver.com>
2022-10-04 19:34:16 +0900

    PG/Lv2/멀리 뛰기 - 2

--- PG/Lv2/멀리 뛰기.js
+++ PG/Lv2/멀리 뛰기.js
@@ -1,20 +1,10 @@
-function getComb(n, r) {
-  let v1 = 1, v2 = 1;
-  for (let i = r; i > 0; i--) {
-      v1 *= n--;
-      v2 *= i;
-      
-  }
-  return v1 / v2;
-}
-
 function solution(n) {
-  let answer = n % 2 === 0 ? 2 : 1;
-
-  for (let i = 1; i < n / 2; i++) {
-      let tmp = n - 2 * i + i; 
-      answer += getComb(tmp, i);
+  let sum = n;
+  let f1 = 1, f2 = 2;
+  for (let i = 3; i <= n; i++) {
+      sum = (f1 % 1234567 + f2 % 1234567) % 1234567;
+      f1 = f2 % 1234567;
+      f2 = sum;
   }
-  
-  return answer % 1234567;
+  return sum;
 }
