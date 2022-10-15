45c3c9b811286353d3201cfe9f371a559c5c550b
HyeJun <solar3070@naver.com>
2022-10-15 20:12:02 +0900

    BOJ/B/별 찍기 - 4

---
+++ BOJ/Bronze/별 찍기 - 4.js
@@ -0,0 +1,10 @@
+function solution(n) {
+  for (let i = n; i >= 1; i--) {
+    console.log("*".repeat(i).padStart(n, " "));
+  }
+}
+
+const fs = require("fs");
+let input = fs.readFileSync("input.txt").toString().trim();
+
+solution(+input);
