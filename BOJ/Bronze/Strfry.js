fcb9d88eff4bcc52ca18821322e761de7c6d9c16
HyeJun <solar3070@naver.com>
2022-10-16 02:56:55 +0900

    BOJ/S3/Strfry - 2

--- BOJ/Bronze/Strfry.js
+++ BOJ/Bronze/Strfry.js
@@ -1,10 +1,33 @@
 function solution(n, arr) {
-  for (let i = 0; i < n; i++) {
-    let s1 = arr[i][0].split("").sort().join("");
-    let s2 = arr[i][1].split("").sort().join("");
+  let answer = "";
 
-    s1 === s2 ? console.log("Possible") : console.log("Impossible");
+  for (let i = 0; i < n; i++) {
+    let s1 = arr[i][0],
+      s2 = arr[i][1];
+
+    if (s1.length !== s2.length) {
+      answer += "Impossible\n";
+    } else {
+      const map1 = new Map();
+      const map2 = new Map();
+      for (let j = 0; j < s1.length; j++) {
+        map1.set(s1[j], (map1.get(s1[j]) || 0) + 1);
+        map2.set(s2[j], (map2.get(s2[j]) || 0) + 1);
+      }
+
+      let possible = 1;
+      for (let [k, v] of map2) {
+        const cnt = map1.get(k);
+        if (cnt === undefined || cnt < v) {
+          possible = 0;
+          break;
+        }
+      }
+      answer += possible ? "Possible\n" : "Impossible\n";
+    }
   }
+
+  return answer;
 }
 
 const fs = require("fs");
@@ -16,4 +39,4 @@ for (let i = 1; i <= n; i++) {
   arr.push(input[i].split(" "));
 }
 
-solution(n, arr);
+console.log(solution(n, arr));
