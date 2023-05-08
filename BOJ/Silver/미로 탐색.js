3835d712177d342a3ee6ec6a0b1499c1caa26ecb
hyejun <solar3070@naver.com>
2023-05-08 16:35:18 +0900

    BOJ/S1/미로탐색

--- BOJ/Silver/미로 탐색.js
+++ BOJ/Silver/미로 탐색.js
@@ -1,19 +1,18 @@
-const fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split('\n');
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item, i) => item.split(i ? "" : " ").map(Number));
+const [[n, m], ...board] = input;
 
-const [n, m] = input[0].split(' ');
-let arr = [];
-for (let i = 1; i <= n; i++) 
-  arr.push(input[i].split('').map((item) => +item));
+solution(n, m, board);
 
-solution(n, m, arr);
+function solution(n, m, board) {
+  const dx = [0, -1, 0, 1];
+  const dy = [-1, 0, 1, 0];
 
-function solution(n, m, arr) {
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
-  
-  let needVisit = [];
-  needVisit.push([0, 0]);
+  const needVisit = [[0, 0]]; // Queue;
 
   while (needVisit.length > 0) {
     const [x, y] = needVisit.shift();
@@ -23,13 +22,12 @@ function solution(n, m, arr) {
       const ny = y + dy[i];
 
       if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-      if (arr[nx][ny] !== 1) continue;
+      if (board[nx][ny] !== 1) continue;
 
-      arr[nx][ny] = arr[x][y] + 1;
       needVisit.push([nx, ny]);
+      board[nx][ny] = board[x][y] + 1;
     }
   }
 
-  console.log(arr[n - 1][m - 1]);
+  console.log(board[n - 1][m - 1]);
 }
-
