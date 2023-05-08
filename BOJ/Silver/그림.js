7b9887580349eb106c6aa84dbc8e41c75f165da7
hyejun <solar3070@naver.com>
2023-05-08 15:38:24 +0900

    BOJ/S1/그림

--- BOJ/Silver/그림.js
+++ BOJ/Silver/그림.js
@@ -1,48 +1,51 @@
-const fs = require('fs');
-let input = fs.readFileSync('input.txt').toString().split('\n');
+const input = require("fs")
+  .readFileSync(process.platform === "linux" ? "/dev/stdin" : "input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((item) => item.split(" ").map(Number));
+const [[n, m], ...board] = input;
 
-const [n, m] = input[0].split(' ');
-let arr = [];
-for (let i = 1; i <= n; i++) 
-  arr.push(input[i].split(' ').map((item) => +item));
+solution(n, m, board);
 
-solution(n, m, arr);
+function solution(n, m, board) {
+  const dx = [0, -1, 0, 1];
+  const dy = [-1, 0, 1, 0];
 
-function solution(n, m, arr) {
-  const dx = [0, 1, 0, -1];
-  const dy = [1, 0, -1, 0];
-  
-  let visited = Array.from({length: n}, () => Array.from({length: m}, () => 0));
-  let needVisit = [];
+  const visited = Array.from(Array(n), () => Array(m).fill(0));
+  const needVisit = []; // Queue
 
-  let cnt = 0, maxArea = 0;
+  let cnt = 0;
+  let maxArea = 0;
 
   for (let i = 0; i < n; i++) {
     for (let j = 0; j < m; j++) {
-      if (arr[i][j] === 1 && !visited[i][j]) {
-        cnt += 1;
+      if (board[i][j] === 1 && visited[i][j] === 0) {
         let area = 1;
+        cnt += 1;
 
         needVisit.push([i, j]);
         visited[i][j] = 1;
 
         while (needVisit.length > 0) {
           const [x, y] = needVisit.shift();
-      
-          for (let i = 0; i < 4; i++) {
-            const nx = x + dx[i];
-            const ny = y + dy[i];
-      
+
+          for (let k = 0; k < 4; k++) {
+            const nx = x + dx[k];
+            const ny = y + dy[k];
+
             if (nx < 0 || ny < 0 || nx >= n || ny >= m) continue;
-            if (arr[nx][ny] !== 1 || visited[nx][ny]) continue;
-      
-            area += 1;
+            if (board[nx][ny] === 0 || visited[nx][ny] === 1) continue;
+
             needVisit.push([nx, ny]);
             visited[nx][ny] = 1;
+            area += 1;
           }
         }
 
-        if (area > maxArea) maxArea = area;
+        if (area > maxArea) {
+          maxArea = area;
+        }
       }
     }
   }
@@ -50,4 +53,3 @@ function solution(n, m, arr) {
   console.log(cnt);
   console.log(maxArea);
 }
-
